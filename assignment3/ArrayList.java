10f438a56f58ef2728fb8e212043ca90f32ecc6c
Nishant Sinha <nishnha@gmail.com>
2017-11-19 19:58:24 -0500

    updated assignment3

--- assignment3/ArrayList.java
+++ assignment3/ArrayList.java
@@ -1,21 +1,46 @@
+/* Nishant Sinha
+ * Bhola Data Structures MW 5:30-6:30
+ * 
+ * This class attempts to recreate the ArrayList class implemented in the java.utils library.
+ * This class is run with the TestArray main method. See TestArray.java for details.
+ * The ArrayList class has 3 private variables: a capacity (defaults to 10), occupied (defaults to 0), and an array of objects (defaults to empty).
+ * The capacity will grow by 1.5 times each time the ArratList reaches the max capacity.
+ * Occupied grows and shrinks with the number of objects that are in the ArrayList.
+ * The array of objects acts simply as a container for the objects passed into the class.
+ * The toString() method of this class will print out the current state of ArrayList.array
+ */
+
 public class ArrayList {
     private int capacity;
     private int occupied = 0;
     private Object[] array;
 
+    /* The default constructor for ArrayList
+     * Initializes an ArrayList with a capacity of 10
+     */
     public ArrayList() {
         this.capacity = 10;
         array = new Object[10];
     }
 
+    /* Overloaded constructor for an ArrayList
+     * Initializes an ArrayList with a passed in capacity
+     * @param capacity must be greater than 0
+     */
     public ArrayList(int capacity) {
         this.capacity = capacity;
         array = new Object[capacity];
     }
 
+    /* Resizes ArrayList.array to hold 1.5 times the amount of objects.
+     * Creates a new array of size (1.5 * ArrayList.capacity) and copies the objects
+     * from the previous ArrayList over. Sets ArrayList.array to point at the new array
+     * and changes ArrayList.capacity to reflect the new capacity.
+     */
     private void resize() {
         int size = (int) (this.array.length * 1.5);
         Object[] output = new Object[size];
+        int prevCapcity = this.capacity;
 
         for (int i = 0; i < this.capacity; i++) {
             output[i] = this.array[i];
@@ -24,17 +49,22 @@ public class ArrayList {
         this.capacity = size;
         this.array = output;
 
-        System.out.println("An array was resized to hold " + this.capacity);
+        System.out.println("ArrayList was resized from " + prevCapcity + " to " + this.capacity + ".");
     }
 
-    // Shift the values in the array to the next bucket by 1 from the given index
-    private void shift(int index) {
+    /* Shift the values in ArrayList.array to the bucket on the right from the given index.
+     * Used as a helper function for add(int, Object).
+     * @param index must be a valid index
+     */
+    private void shiftR(int index) {
         for (int i = this.size() + 1; i > index; i--) {
             this.array[i] = this.array[i - 1];
         }
     }
 
-
+    /* Appends the passed in Object to the end of the ArrayList
+     * Resizes the ArrayList if ArrayList.array is too small
+     */
     public void add(Object o) {
         if (occupied < capacity) {
             this.occupied += 1;
@@ -46,31 +76,36 @@ public class ArrayList {
         }
     }
 
+    /* Adds the passed in Object at the passed in index.
+     * Shifts ArrayList.array so that no objects are overwritten.
+     * Resizes the ArrayList if ArrayList.array is too small.
+     */
     public void add(int index, Object o) {
-            this.array[index] = o;
-    }
-
-    public void insert(int index, Object o) {
         if (occupied < capacity) {
             this.occupied += 1;
-            this.shift(index);
+            this.shiftR(index);
             this.array[index] = o;
         } else {
             this.resize();
             this.occupied += 1;
-            this.shift(index);
+            this.shiftR(index);
             this.array[index] = o;
         }
     }
 
+    /* Returns the object at the passed in index.
+     * @param index must be greater than 0
+     */
     public Object get(int index) {
         return this.array[index];
     }
 
+    /* Returns the number of occupied slots in the ArrayList */
     public int size() {
         return this.occupied;
     }
 
+    /* Returns whether the passed in object is in the ArrayList */
     public boolean isIn(Object o) {
         for (int i = 0; i < this.array.length; i++)
             if (this.array[i] == o)
@@ -79,6 +114,9 @@ public class ArrayList {
         return false;
     }
 
+    /* Returns the index of the first occurance of the passed in object in the ArrayList.
+     * Returns -1 if the object is not in the ArrayList.
+      */
     public int find(Object o) {
         for (int i = 0; i < this.array.length; i++)
             if (this.array[i] == o)
@@ -86,27 +124,47 @@ public class ArrayList {
         return -1;
     }
 
+    /* Returns true if the ArrayList is empty */
     public boolean isEmpty() {
         return this.size() == 0;
     }
 
+    /* Shift the values in ArrayList.array to the bucket on the left from the given index.
+     * Used as a helper function for remove(Object).
+     * @param index must be a valid index
+     */
+    private void shiftL(int index) {
+        for (int i = index; i < this.occupied; i++) {
+            this.array[i] = this.array[i + 1];
+        }
+    }
+
+    /* Removes the passed in object from the ArrayList.
+     * shifts Objects in the ArrayList one bucket left
+     * from the first occurance that matches the passed in Object,
+     * sets the last object as 0, and decreases the size of the ArrayList by 1.
+    */
     public void remove(Object o) {
         for (int i = 0; i < this.array.length; i++) {
             if (this.array[i] == o) {
-                this.array[i] = null;
+                this.shiftL(i);
+                this.array[this.occupied] = 0;
                 this.occupied -= 1;
             }
         }
     }
 
+    /* Returns a String with the contents of ArrayList.array */
     @Override
     public String toString() {
         String output = "";
-        for (int i = 0; i < this.size(); i++) {
+        
+        for (int i = 0; i < this.size(); i++)
             output = output + this.get(i) + ", ";
-        }
+
         // Chop off the extra comma
-        output = output.substring(0, output.length() - 2);
+        if (output.length() > 2)
+            output = output.substring(0, output.length() - 2);
 
         return output;
     }
--- assignment3/TestArray.java
+++ assignment3/TestArray.java
@@ -1,17 +1,29 @@
+/* Nishant Sinha
+ * Bhola Data Structures MW 5:30-6:30
+ * 
+ * This program contains the main method used to test the ArrayList class.
+ * This program has two methods: the main method used to test each method in the ArrayList class for each type of constructor
+ * and the randomInt() method that generates a random integer between 1 and 25.
+ * The only data structure used in this program is the ArrayList class.
+ * Run the program through the console `java TestArray`
+ */
+
 import java.util.Random;
 
 public class TestArray {
 
-    private static final int ARRAYLIST_SIZE = 15;
+    private static final int TEST_SIZE = 15;
 
     public static void main(String[] args) {
 
-        // Initialize two ArrayLists.
+        // Initialize two ArrayLists using both constructors
+        System.out.println("Creating ArrayList a and ArrayList b");
         ArrayList a = new ArrayList();
-        ArrayList b = new ArrayList(ARRAYLIST_SIZE);
+        ArrayList b = new ArrayList(TEST_SIZE);
 
-        // Populate the ArrayLists with 15 random integers
-        for (int i = 0; i < ARRAYLIST_SIZE; i++) {
+        // Populate the ArrayLists with TEST_SIZE random integers
+        System.out.println("Populating ArrayList a and ArrayList b");
+        for (int i = 0; i < TEST_SIZE; i++) {
             a.add(randomInt());
             b.add(randomInt());
         }
@@ -22,60 +34,70 @@ public class TestArray {
         System.out.println();
 
         // Testing the add method
-        System.out.println("Adding a new random value to each array: ");
+        System.out.println("Adding a new random value to each array using the add(Object) method: ");
         a.add(randomInt());
-        b.add(randomInt());
         System.out.println("a: " + a.toString());
+        b.add(randomInt());
         System.out.println("b: " + b.toString());
         System.out.println();
 
         // Testing the overloaded add method
-        System.out.println("Adding a new random value to each array: ");
+        System.out.println("Adding a new random value to each array using the add(int, Object) method: ");
         a.add(0, randomInt());
-        b.add(1, randomInt());
         System.out.println("a: " + a.toString());
-        System.out.println("b: " + b.toString());
-        System.out.println();
-
-        // Testing the insert method
-        System.out.println("Inserting a new random value to each array: ");
-        a.insert(0, randomInt());
-        b.insert(1, randomInt());
-        System.out.println("a: " + a.toString());
+        b.add(0, randomInt());
         System.out.println("b: " + b.toString());
         System.out.println();
 
         // Testing the isIn and find methods
         int testIsIn = randomInt();
-
-        String isInA = a.isIn(testIsIn) ? " is in a" : " is not in a";
-        System.out.println(testIsIn + isInA + " at index " + a.find(testIsIn));
-
-        String isInB = b.isIn(testIsIn) ? " is in b" : " is not in b";
-        System.out.println(testIsIn + isInB +  " at index " + a.find(testIsIn));
+        System.out.println("Testing the iSIn(int) method:");
+        System.out.print("Checking if " + testIsIn + " is in a: ");
+        System.out.println(a.isIn(testIsIn));
+        System.out.print("Checking if " + testIsIn + " is in b: ");
+        System.out.println(b.isIn(testIsIn));
         System.out.println();
 
+        // Testing the find method
+        System.out.println("Testing the find(Object) method:");
+        System.out.println(testIsIn + " is in a at index: " + a.find(testIsIn));
+        System.out.println(testIsIn + " is in b at index: " + b.find(testIsIn));
+        System.out.println();
+        
         // Testing the get methods
-        Random rand = new Random();
-        int randIndex = (int) (rand.nextFloat() * ARRAYLIST_SIZE);
-        System.out.println("The object in a at index " + randIndex + " is " + a.get(randIndex));
-        System.out.println("The object in b at index " + randIndex + " is " + b.get(randIndex));
+        System.out.println("Testing the get(int) method:");
+        System.out.println("The object in a at index " + TEST_SIZE + " is " + a.get(TEST_SIZE));
+        System.out.println("The object in b at index " + TEST_SIZE + " is " + b.get(TEST_SIZE));
         System.out.println();
 
         // Testing the size method
+        System.out.println("Testing the size() method:");
         System.out.println("ArrayList a has " + a.size() + " objects.");
         System.out.println("ArrayList b has " + b.size() + " objects.");
         System.out.println();
 
         // Testing the remove method
-        System.out.println("Removing all objects from a");
-        for (int i = 0; i < a.size(); i++)
-            a.remove(i);
+        System.out.println("Testing the remove(int) method:");
+        System.out.println("Removing all objects from a:");
+        // For each bucket in a.array, try removing an integers 1-25
+        for (int i = 0; i < a.size(); i++) {
+            for (int j = 1; j < 26; j++) {
+                a.remove(j);
+            }
+        }
         System.out.println("a: " + a.toString());
+        
+        System.out.println("Removing the first Object in b:");
+        int firstInB = (int) b.get(0);
+        b.remove(firstInB);
+        System.out.println("b: " + b.toString());
+        
         System.out.println();
 
         // Testing the isEmpty method
+        System.out.println("Testing the isEmpty() method:");
         System.out.println("ArrayList a is empty: " + a.isEmpty());
+        System.out.println("ArrayList b is empty: " + b.isEmpty());
         System.out.println();
 
     }
